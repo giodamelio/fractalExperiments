c1e80c7d5d7a63864c229f2a1e1f7412470665e8
Gio d'Amelio <giodamelio@gmail.com>
2014-09-07 17:16:23 -0700

    Quad dragon curve

--- dragonCurve.py
+++ dragonCurve.py
@@ -10,37 +10,57 @@ window.screensize(10000, 10000)
 window.tracer(128, 1)
 
 # Create first turtle
-dragon = turtle.Turtle()
-dragon.color("#FF4136")
-dragon.hideturtle()
+dragon1 = turtle.Turtle()
+dragon1.color("#FF4136")
+dragon1.hideturtle()
 
 # Create second turtle
 dragon2 = turtle.Turtle()
 dragon2.color("#2ECC40")
 dragon2.hideturtle()
-dragon2.left(180)
+dragon2.left(90)
+
+# Create third turtle
+dragon3 = turtle.Turtle()
+dragon3.color("#0074D9")
+dragon3.hideturtle()
+dragon3.left(180)
+
+# Create forth turtle
+dragon4 = turtle.Turtle()
+dragon4.color("#FFDC00")
+dragon4.hideturtle()
+dragon4.left(270)
 
 # Draw our dragon
 short = False
 for n in range(1, 10000):
     # Move forward
     if short:
-        dragon.forward(3)
-        dragon2.forward(3)
-    else:
-        dragon.forward(5)
+        dragon1.forward(3)
         dragon2.forward(5)
+        dragon3.forward(3)
+        dragon4.forward(5)
+    else:
+        dragon1.forward(5)
+        dragon2.forward(3)
+        dragon3.forward(5)
+        dragon4.forward(3)
 
     # Toggle forward distance
     short = not short
 
     # Determine weather we should turn right or left
     if (((n & -n) << 1) & n) != 0:
-        dragon.left(90)
+        dragon1.left(90)
         dragon2.left(90)
+        dragon3.left(90)
+        dragon4.left(90)
     else:
-        dragon.right(90)
+        dragon1.right(90)
         dragon2.right(90)
+        dragon3.right(90)
+        dragon4.right(90)
 
 # Wait for the user to click to exit
 window.exitonclick()
